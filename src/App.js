796a84dbdcc69c8242a7949b03815a6b8af7c924
r3dDoX <patrick@walther-online.ch>
2017-09-26 21:22:27 +0200

    install styled-components and transform first components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import logo from './logo.svg';
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const Logo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+`;
+
+const Intro = styled.p`
+  font-size: large;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppContainer>
+        <Header>
+          <Logo src={logo} alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        </Header>
+        <Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </Intro>
+      </AppContainer>
     );
   }
 }
